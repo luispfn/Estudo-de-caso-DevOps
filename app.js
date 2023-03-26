50662e542b330979fd07562262dc649d4687b5cc
luispfn <128197345+luispfn@users.noreply.github.com>
2023-03-26 13:28:57 -0300

    Criação do Arquivo app.js
    Criação do arquivo app.js para a atividade estudo de casos de DevOps

---
+++ app.js
@@ -0,0 +1,9 @@
+// Criando um objeto pessoa
+var pessoa = {
+  nome: "João",
+  idade: 30
+};
+
+// Imprimindo as informações da pessoa no console
+console.log("Nome: " + pessoa.nome);
+console.log("Idade: " + pessoa.idade);
